01e878f338e0a1eae9af0fa627f47fecde27c382
joncomo <jon@underground.net>
2015-12-08 14:04:47 -0500

    Initial Commit

---
+++ thirdEye/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  thirdEye
+//
+//  Created by Jon Como on 12/8/15.
+//  Copyright © 2015 Jon Como. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ thirdEye/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  thirdEye
+//
+//  Created by Jon Como on 12/8/15.
+//  Copyright © 2015 Jon Como. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
